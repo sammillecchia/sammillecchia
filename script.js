5deec338924d27a79e311e5297cd0182ee8d24ed
Sam Millecchia <sammillecchia@gmail.com>
2023-10-10 13:41:29 -0400

    Commit 8 :)

--- script.js
+++ script.js
@@ -1,18 +1,24 @@
-function myFunction() {
-    document.getElementById("test").innerHTML="I feel like a wizard ngl";
+function Bwompening() {
+    playAudio();
+    imageTest();
 }
 
-let mySound = new Audio("bwomp.mp3")
+let bwompimage = document.getElementById('image');
+function imageTest() {
+     image.src = "bwompimg.jpg";
+     document.getElementById("buttonid").style.display = "none";
+     document.getElementById("image").style.display = "block";
+     document.getElementById("image").style.animation = "fade-in 3s forwards";
+}
 
-
+let mySound = new Audio("bwomp.mp3");
 var played = 0;
 function playAudio() {
 
 
     if (played == 0) {
-        mySound.play()
+        mySound.play();
         played++;
     }
       
 }
-
