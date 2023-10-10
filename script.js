49f939cf02b0f2481ac87a90386661e0ed4c8b43
Sam Millecchia <sammillecchia@gmail.com>
2023-10-10 03:04:50 -0400

    Commit 7, Fixed sound?

--- script.js
+++ script.js
@@ -1,3 +1,9 @@
 function myFunction() {
     document.getElementById("test").innerHTML="I feel like a wizard ngl";
 }
+
+let mySound = new Audio("bwomp.mp3")
+
+function playAudio() {
+    mySound.play()
+}
